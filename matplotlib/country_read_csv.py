39cf7d8b4a5150fdb73bc299f5c171be65bf03fa
Prabin Neupane <neupaneprabin2058@gmail.com>
2025-03-18 11:27:27 +0000

    histogram1st

--- matplotlib/country_read_csv.py
+++ matplotlib/country_read_csv.py
@@ -7,18 +7,20 @@ csv_path = os.path.join(parent_dir,'country_data.csv')
 # form .. import country_data as file
 # df = pd.read_csv('../country_data.csv') #this import only when running the script form the same directly as it is relative improt
 df = pd.read_csv(csv_path)
+df_cleaned = df.dropna()
 # print(df.head())
-gdp_cap = df['gdp'].tolist() #convert any column of df to lis
-
-life_exp_male = df['life_expectancy_male'].tolist()
+gdp_cap = df_cleaned['gdp_per_capita'].tolist() #convert any column of df to lis
+pop = df_cleaned['population'].tolist()
+life_exp_male = df_cleaned['life_expectancy_male'].tolist()
 
 # data_dict = df.to_dict(orient= 'list')  convert the df into dict with header as key and all the column values as values
-#orient = 'records' converts df into list of dictionaries with header/column name as key
+#Alternative:: orient = 'records' converts df into list of dictionaries with header/column name as key
 # print(data_dict)
 # print(df.iloc[:,0:5])   index based slicing of df  other are .loc and list of columns df[['c1','c2','c3']]
 # life_exp = df['']
-print(gdp_cap[0:3],life_exp_male[0:3])
-
-plt.scatter(gdp_cap[0:3],life_exp_male[0:3])    #only used 3 points because data cleaning is not done
-plt.xscale('log')
-plt.show()
+# print(gdp_cap[0:10],life_exp_male[0:10])
+if __name__ == '__main__':
+    print(df_cleaned.columns)
+    plt.scatter(gdp_cap[0:100],life_exp_male[0:100])    #only used 3 points because data cleaning is not done
+    plt.xscale('log')
+    plt.show()
---
+++ matplotlib/histogram1.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt 
+from country_read_csv import gdp_cap,life_exp_male,pop
+
+plt.hist(life_exp_male)
+plt.show()
